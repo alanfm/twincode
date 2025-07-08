a380465be90c4deb13536d9ef8bcd69e43b49e29
Alan Freire <alan.freire.87@gmail.com>
2025-07-08 12:26:19 -0300

    Correção para produção

--- vite.config.js
+++ vite.config.js
@@ -16,14 +16,6 @@ export default defineConfig({
         tailwindcss(),
     ],
     build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
+        chunkSizeWarningLimit: 1500, // Aumenta o limite para 1.5MB
     }
 });
