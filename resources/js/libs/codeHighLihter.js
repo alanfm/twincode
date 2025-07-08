0134909e08c9a9134a687ff98d8464946ccdcc8c
Alan Freire <alan.freire.87@gmail.com>
2025-07-08 12:11:59 -0300

    Fix codeHighLighter

--- resources/js/libs/codeHighLihter.js
+++ resources/js/libs/codeHighLihter.js
@@ -1,10 +1,21 @@
 import { createHighlighter } from 'shiki';
 
-const highlighter = await createHighlighter({
+let highlighter = null;
+
+// Pré-carrega o highlighter no momento da inicialização
+createHighlighter({
     themes: ['github-light'],
     langs: ['javascript', 'css', 'html', 'java', 'php', 'python', 'ruby', 'csharp', 'typescript', 'go', 'rust', 'bash', 'json', 'xml', 'sql'],
+}).then((hl) => {
+    highlighter = hl;
+}).catch((err) => {
+    console.error('Erro ao inicializar o highlighter:', err);
 });
 
 export default function codeToHtml(code, lang, theme = 'github-light') {
-    return highlighter.codeToHtml(code, { lang: lang, theme: theme })
+    if (!highlighter) {
+        // Retorna um fallback caso o highlighter ainda não tenha carregado
+        return `<pre><code>${code}</code></pre>`;
+    }
+    return highlighter.codeToHtml(code, { lang: lang, theme: theme });
 }
