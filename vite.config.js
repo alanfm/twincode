ca035ef7b3e8dfc494e2b997eb89656a110ffc49
Alan Freire <alan.freire.87@gmail.com>
2025-07-08 12:20:40 -0300

    Fix build grandes arquivos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({    
+export default defineConfig({
     esbuild: {
         target: 'esnext', // <--- permite top-level await
     },
@@ -15,4 +15,15 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
